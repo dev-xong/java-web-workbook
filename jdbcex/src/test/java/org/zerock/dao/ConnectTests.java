5331a9ceb41b561e1dc84dbd0b33c03161e10a66
yunaxong <songyoona1209@gmail.com>
2025-05-18 22:07:07 +0900

    jdbc Connection Test

---
+++ jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
@@ -0,0 +1,25 @@
+package org.zerock.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectTests {
+
+    @Test
+    public void testConnect()  throws Exception {
+
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/webdb",
+                "webuser",
+                "0304");
+
+                Assertions.assertNotNull(connection);
+
+                connection.close();
+    }
+}
