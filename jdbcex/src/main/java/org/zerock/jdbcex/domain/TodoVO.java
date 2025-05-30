f706b66758416eb93a9353a3369255bfb6966a0c
yunaxong <songyoona1209@gmail.com>
2025-05-30 23:35:59 +0900

    Lombok 추가

---
+++ jdbcex/src/main/java/org/zerock/jdbcex/domain/TodoVO.java
@@ -0,0 +1,19 @@
+package org.zerock.jdbcex.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@ToString
+public class TodoVO {
+
+    private Long tno;
+    private String title;
+    private LocalDateTime dueDate;
+    private boolean finished;
+
+}
