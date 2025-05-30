aa5424fb99829dac1e7f6a7ca9298fb6eade0234
yunaxong <songyoona1209@gmail.com>
2025-05-31 00:50:39 +0900

    HikariCP 추가

--- jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
+++ jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
@@ -1,5 +1,7 @@
 package org.zerock.dao;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +23,27 @@ public class ConnectTests {
                 Assertions.assertNotNull(connection);
 
                 connection.close();
+    }
+
+    @Test
+    public void testHicariCP() throws Exception {
+
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.mariadb.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
+        config.setUsername("webuser");
+        config.setPassword("0304");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource ds = new HikariDataSource(config);
+        Connection connection = ds.getConnection();
+
+        System.out.println(connection);
+
+        connection.close();
+
+
     }
 }
