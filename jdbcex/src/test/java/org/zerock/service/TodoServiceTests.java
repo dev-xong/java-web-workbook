a5bcd71ea1606049c676b25752403ee10b0fdbb0
xongyuna <123929671+dev-xong@users.noreply.github.com>
2025-07-29 21:44:11 +0900

    [PART 2.3] Log4j2 Test

--- jdbcex/src/test/java/org/zerock/service/TodoServiceTests.java
+++ jdbcex/src/test/java/org/zerock/service/TodoServiceTests.java
@@ -1,5 +1,6 @@
 package org.zerock.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.zerock.jdbcex.dto.TodoDTO;
@@ -7,6 +8,7 @@ import org.zerock.jdbcex.service.TodoService;
 
 import java.time.LocalDate;
 
+@Log4j2
 public class TodoServiceTests {
 
     private TodoService todoService;
@@ -23,7 +25,10 @@ public class TodoServiceTests {
                 .dueDate(LocalDate.now())
                 .build();
 
-                todoService.register(dto);
+        log.info("----------------------------");
+        log.info(dto);
+
+        todoService.register(dto);
     }
 
 }
