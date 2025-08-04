ea6617fbd4acaafdbf1a3264be0425fab454db95
xongyuna <123929671+dev-xong@users.noreply.github.com>
2025-08-04 22:42:45 +0900

    [PART 3.1] Login로직 sesstion 확인

--- w2/src/main/java/org/zerock/w2/controller/TodoRegisterController.java
+++ w2/src/main/java/org/zerock/w2/controller/TodoRegisterController.java
@@ -28,6 +28,8 @@ public class TodoRegisterController  extends HttpServlet {
 
         HttpSession session = req.getSession();
 
+        String contextPath = req.getContextPath();
+
         if(session.isNew()){
             log.info("JSESSIONID 쿠키가 새로 만들어진 사용자");
             //resp.sendRedirect("/Login");
@@ -35,7 +37,7 @@ public class TodoRegisterController  extends HttpServlet {
         }
         if(session.getAttribute("loginInfo") == null){
             log.info("로그인한 정보가 없는 사용자");
-            //resp.sendRedirect("/Login");
+            resp.sendRedirect(contextPath + "/login");
             return;
         }
 
