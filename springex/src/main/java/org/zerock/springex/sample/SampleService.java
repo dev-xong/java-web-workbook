ddc490095bc859cf24f96bfb6462f644f011796a
xongyuna <123929671+dev-xong@users.noreply.github.com>
2025-08-20 17:04:14 +0900

    [PART 4.1] 생성자 주입 -- @Qualifier -- @Primary

--- springex/src/main/java/org/zerock/springex/sample/SampleService.java
+++ springex/src/main/java/org/zerock/springex/sample/SampleService.java
@@ -3,6 +3,7 @@ package org.zerock.springex.sample;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +11,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SampleService {
 
+    @Qualifier("normal")
     private final SampleDAO sampleDAO;
+
 }
