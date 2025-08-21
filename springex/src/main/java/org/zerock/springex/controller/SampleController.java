0312b2d7ac0950eb2590e386e7155fe772342f1c
xongyuna <123929671+dev-xong@users.noreply.github.com>
2025-08-21 15:51:04 +0900

    [PART4] Controller Test

---
+++ springex/src/main/java/org/zerock/springex/controller/SampleController.java
@@ -0,0 +1,16 @@
+package org.zerock.springex.controller;
+
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Log4j2
+@Controller
+public class SampleController {
+
+    @GetMapping("/hello")
+    public void hello(){
+        log.info("hello..............");
+    }
+}
