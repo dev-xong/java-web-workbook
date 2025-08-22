c613a7f66713da9f15502e2374ffd68defe56a7b
xongyuna <123929671+dev-xong@users.noreply.github.com>
2025-08-22 22:43:34 +0900

    [PART4-3] 스프링 MVC 범용적인 예외처리

--- springex/src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
+++ springex/src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+
 @ControllerAdvice
 @Log4j2
 public class CommonExceptionAdvice {
@@ -17,5 +19,25 @@ public class CommonExceptionAdvice {
         log.error(numberFormatException.getMessage());
 
         return "NUMBER FORMAT EXCEPTION";
+    }
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public String exceptionCommon(Exception exception){
+
+        log.error("--------------------------------");
+        log.error(exception.getMessage());
+
+        StringBuffer buffer = new StringBuffer("<ul>");
+
+        buffer.append("<li>"+ exception.getMessage() +"</li>");
+
+        Arrays.stream(exception.getStackTrace()).forEach(stackTraceElement -> {
+            buffer.append("<li>"+ stackTraceElement.toString() +"</li>");
+        });
+        buffer.append("</ul>");
+
+        return buffer.toString();
+
     }
 }
