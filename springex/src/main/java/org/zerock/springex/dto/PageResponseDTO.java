db43022344f10d52b49cd812354fce4e841a5b26
xongyuna <123929671+dev-xong@users.noreply.github.com>
2025-09-21 00:08:20 +0900

    [PART4-4] 페이징 처리 진행중

---
+++ springex/src/main/java/org/zerock/springex/dto/PageResponseDTO.java
@@ -0,0 +1,30 @@
+package org.zerock.springex.dto;
+
+import lombok.Builder;
+
+import java.util.List;
+
+public class PageResponseDTO<E> {
+
+    private int page;
+    private int size;
+    private int total;
+
+    private int start;
+    private int end;
+
+    private boolean prev;
+    private boolean next;
+
+    private List<E> dtoList;
+
+    @Builder(builderMethodName = "withAll")
+    public PageResponseDTO(PageRequestDTO pageRequestDTO, List<E> dtoList, int total) {
+
+        this.page = pageRequestDTO.getPage();
+        this.size = pageRequestDTO.getSize();
+
+        this.total = total;
+        this.dtoList = dtoList;
+    }
+}
--- springex/src/main/java/org/zerock/springex/mapper/TodoMapper.java
+++ springex/src/main/java/org/zerock/springex/mapper/TodoMapper.java
@@ -1,6 +1,8 @@
 package org.zerock.springex.mapper;
 
 import org.zerock.springex.domain.TodoVO;
+import org.zerock.springex.dto.PageRequestDTO;
+import sun.jvm.hotspot.debugger.Page;
 
 import java.util.List;
 
@@ -17,5 +19,9 @@ public interface TodoMapper {
     void delete(Long tno);
 
     void update(TodoVO todoVO);
+
+    List<TodoVO> selectList(PageRequestDTO pageRequestDTO);
+
+    int getCount(PageRequestDTO pageRequestDTO);
 
 }
